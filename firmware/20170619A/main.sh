2b006eca719b990c027c91ed256fc3bf5645d888
Glushchenko Alexey <glushchenko.alexey@gmail.com>
2017-06-19 18:08:51 +0300

    Update main.sh

--- firmware/20170619A/main.sh
+++ firmware/20170619A/main.sh
@@ -5,7 +5,9 @@ then
 	wget --no-cache --no-check-certificate -O /tmp/firmware.bin https://github.com/GlshchnkLx/OpenWRT_Firmware_Installer/raw/master/firmware/20170619A/openwrt-ar71xx-generic-oolite-squashfs-sysupgrade.bin
 	sleep 10
 	uci set firmware_installer.this.version_install="20170619A"
-	uci set firmware_installer.this.data_install=date +"%Y.%m.%d %k:%M"
+	
+	temp=$(date +"%Y.%m.%d %k:%M")
+	uci set firmware_installer.this.data_install="$temp"
 	uci commit firmware_installer
 	sysupgrade -F -v /tmp/firmware.bin
 fi
